86f93422ef6de15da8eedc006afa773615d92286
kopak011 <165542474+kopak011@users.noreply.github.com>
2024-03-31 13:46:37 +0800

    Add files via upload

---
+++ data/BANGUNANCALONPELANGGANMANDIRI_10.js
@@ -0,0 +1,1 @@
+var json_BANGUNANCALONPELANGGANMANDIRI_10 = {"type":"FeatureCollection","name":"BANGUNANCALONPELANGGANMANDIRI_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
